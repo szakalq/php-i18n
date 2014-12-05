fe637fd61b84c91ce4cf715c33dc8c4d40d3783b
Rafael <szakalq@gmail.com>
2014-12-05 09:59:34 +0100

    init
    add composer support

---
+++ README.md
@@ -0,0 +1,176 @@
+Original project: https://github.com/Philipp15b/php-i18n
+
+# PHP i18n
+This is a simple i18n class for PHP. Nothing fancy, but fast, because it uses caching and it is easy to use. Try it out!
+
+Some of its features:
+
+* Translations in ini-files
+* File caching
+* Simple API (`L::category_stringname`)
+* Automatic finding out what language to use
+* Simplicity ;)
+
+## Requirements
+
+* Write permissions in cache directory
+* PHP 5.2 (only tested with this version, it maybe runs with other versions too)
+* PHP SPL extension (installed by default)
+
+## Install
+```json
+"require": {
+    "szakalq/php-i18n": "dev-master"
+}
+```
+
+## Setup
+To use the i18n class, look at the example.php. You will find there a simple tutorial for this class in the file. Otherwise follow these easy five steps:
+
+### 1. Create language files
+To use this class, you have to use ini files for the translated strings. This could look like this:
+
+`lang_en.ini` (English)
+
+```ini
+greeting = "Hello World!"
+
+[category]
+somethingother = "Something other..."
+```
+
+`lang_de.ini` (German)
+
+```ini
+greeting = "Hallo Welt!"
+
+[category]
+somethingother = "Etwas anderes..."
+```
+
+Save both files in the directory you will set in step 4. 
+The files must be named like the filePath setting, where '{LANGUAGE}' will be replaced by the chosen language, e.g. 'en' or 'de'.
+
+### 2. Include the class
+```php
+<?php
+	require_once 'lib/i18n.class.php';
+?>
+```
+
+### 3. Initialize the class
+```php
+<?php
+	$i18n = new i18n();
+?>
+```
+
+### 4. Set some settings if necessary
+The possible settings are:
+
+* Language file path (the ini files) (default: `./lang/lang_{LANGUAGE}.ini`)
+* Cache file path (default: `./langcache/`)
+* The fallback language, if no one of the user languages is available (default: `en`)
+* A forced language, if you want to force a language (default: none)
+* The ini section seperator: this is used to seperate the sections in the language class. If you set the seperator to `_abc_` you could access your localized strings via `L::category_abc_stringname` if you use categories in your ini. (default: `_`)
+
+```php
+<?php
+	$i18n->setCachePath('./tmp/cache');
+	$i18n->setFilePath('./langfiles/lang_{LANGUAGE}.ini'); // language file path (the ini files)
+	$i18n->setFallbackLang('en');
+	$i18n->setForcedLang('en'); // force english, even if another user language is available
+	$i18n->setSectionSeperator('_');
+?>
+```
+
+#### Shorthand
+There is also a shorthand for that: you can set all settings in the constructor!
+
+```php
+<?php
+	$i18n = new i18n('lang/lang_{LANGUAGE}.ini', 'langcache/', 'en');
+?>
+```
+
+The parameters are:
+
+1. the language file path (the ini files)
+2. the language cache path
+3. fallback language
+4. forced language
+5. ini section seperator
+
+... exactly in this order (all parameters are optional).
+
+### 5. Call the `init()` method to load all files and translations
+Call the `init()` file to instruct the class to load the needed language file, to load the cache file or generate it  if it is not available and make the `L` class available so you can access your localizations.
+
+```php
+<?php
+	$i18n->init();
+?>
+```
+
+### 6. Use the localizations
+To call your localizations, simple use the `L` class and a class constant for the string.
+
+In this example, we use the translation string seen in step 1.
+
+```php
+<?php
+	echo L::greeting;
+	// If 'en' is applied: 'Hello World'
+
+	echo L::category_somethingother;
+	// If 'en' is applied: 'Something other...'
+?>
+```
+
+Thats it!
+
+## How it finds out the user language
+This class tries to find out the user language by generating a queue of the following things:
+
+1. Forced language (if set)
+2. GET parameter 'lang' (`$_GET['lang']`)
+3. SESSION parameter 'lang' (`$_SESSION['lang']`)
+4. HTTP_ACCEPT_LANGUAGE (can be multiple languages) (`$_SERVER['HTTP_ACCEPT_LANGUAGE']`)
+5. Fallback language
+
+First it will remove duplicate elements and then it will replace all characters that are not A-Z, a-z or 0-9. 
+After that it searches for the language files. For example, if you set the GET parameter 'lang' to 'en' without a forced language set, the class would try to find the file `lang/lang_en.ini` (if the setting `langFilePath` was set to default (`lang/lang_{LANGUAGE}.ini`)).
+If this file was not there, it would try to find the language file for the language defined in the session and so on.
+
+### How to change this implementation
+You can change this 'algorithm' by extending the i18n class. You could do it like that:
+
+```php
+<?php	
+	require_once 'lib/i18n.class.php';
+	class My_i18n extends i18n {
+		
+		public function getUserLangs() {
+			$userLangs = new array();
+
+			$userLangs[] = $_GET['language'];
+
+			$userLangs[] = $_SESSION['userlanguage'];
+
+			return $userLangs;
+		}
+
+	}
+
+	$i18n = new My_i18n();
+	// [...]
+?>
+```
+
+This very basic extension of the i18n class replaces the default implementation of the `getUserLangs()`-method and only uses the GET parameter 'language' and the session parameter 'userlanguage'.
+You see that this method must return an array.
+
+Note that this is script insecure. Dont use it in a production environment! You should escape all user data!
+
+## Fork it!
+Please fork this project and help me with the development.
---
+++ src/i18n.class.php
@@ -0,0 +1,293 @@
+<?php
+
+/*
+ * Fork this project on GitHub!
+ * https://github.com/Philipp15b/php-i18n
+ *
+ * License: Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ * License URL: http://creativecommons.org/licenses/by-sa/3.0/deed.en
+ */
+
+class i18n {
+
+    /**
+     * Language file path
+     * This is the path for the language files. You must use the '{LANGUAGE}' placeholder for the language or the script wont find any language files.
+     *
+     * @var string
+     */
+    protected $filePath = './lang/lang_{LANGUAGE}.ini';
+
+    /**
+     * Cache file path
+     * This is the path for all the cache files. Best is an empty directory with no other files in it.
+     *
+     * @var string
+     */
+    protected $cachePath = './langcache/';
+
+    /**
+     * Fallback language
+     * This is the language which is used when there is no language file for all other user languages. It has the lowest priority.
+     * Remember to create a language file for the fallback!!
+     *
+     * @var string
+     */
+    protected $fallbackLang = 'en';
+
+    /**
+     * The class name of the compiled class that contains the translated texts.
+     * @var string
+     */
+    protected $prefix = 'L';
+
+    /**
+     * Forced language
+     * If you want to force a specific language define it here.
+     *
+     * @var string
+     */
+    protected $forcedLang = NULL;
+
+    /**
+     * This is the seperator used if you use sections in your ini-file.
+     * For example, if you have a string 'greeting' in a section 'welcomepage' you will can access it via 'L::welcomepage_greeting'.
+     * If you changed it to 'ABC' you could access your string via 'L::welcomepageABCgreeting'
+     *
+     * @var string
+     */
+    protected $sectionSeperator = '_';
+
+
+    /*
+     * The following properties are only available after calling init().
+     */
+
+    /**
+     * User languages
+     * These are the languages the user uses.
+     * Normally, if you use the getUserLangs-method this array will be filled in like this:
+     * 1. Forced language
+     * 2. Language in $_GET['lang']
+     * 3. Language in $_SESSION['lang']
+     * 4. Fallback language
+     *
+     * @var array
+     */
+    protected $userLangs = array();
+
+    protected $appliedLang = NULL;
+    protected $langFilePath = NULL;
+    protected $cacheFilePath = NULL;
+    protected $isInitialized = false;
+
+
+    /**
+     * Constructor
+     * The constructor sets all important settings. All params are optional, you can set the options via extra functions too.
+     *
+     * @param string [$filePath] This is the path for the language files. You must use the '{LANGUAGE}' placeholder for the language.
+     * @param string [$cachePath] This is the path for all the cache files. Best is an empty directory with no other files in it. No placeholders.
+     * @param string [$fallbackLang] This is the language which is used when there is no language file for all other user languages. It has the lowest priority.
+     * @param string [$prefix] The class name of the compiled class that contains the translated texts. Defaults to 'L'.
+     */
+    public function __construct($filePath = NULL, $cachePath = NULL, $fallbackLang = NULL, $prefix = NULL) {
+        // Apply settings
+        if ($filePath != NULL) {
+            $this->filePath = $filePath;
+        }
+
+        if ($cachePath != NULL) {
+            $this->cachePath = $cachePath;
+        }
+
+        if ($fallbackLang != NULL) {
+            $this->fallbackLang = $fallbackLang;
+        }
+
+        if ($prefix != NULL) {
+            $this->prefix = $prefix;
+        }
+    }
+
+    public function init() {
+        if ($this->isInitialized()) {
+            throw new BadMethodCallException('This object from class ' . __CLASS__ . ' is already initialized. It is not possible to init one object twice!');
+        }
+
+        $this->isInitialized = true;
+
+        $this->userLangs = $this->getUserLangs();
+
+        // search for language file
+        $this->appliedLang = NULL;
+        foreach ($this->userLangs as $priority => $langcode) {
+            $this->langFilePath = str_replace('{LANGUAGE}', $langcode, $this->filePath);
+            if (file_exists($this->langFilePath)) {
+                $this->appliedLang = $langcode;
+                break;
+            }
+        }
+        if ($this->appliedLang == NULL) {
+            throw new RuntimeException('No language file was found.');
+        }
+
+        // search for cache file
+        $this->cacheFilePath = $this->cachePath . '/php_i18n_' . md5_file(__FILE__) . '_' . $this->appliedLang . '.cache.php';
+
+        // if no cache file exists or if it is older than the language file create a new one
+        if (!file_exists($this->cacheFilePath) || filemtime($this->cacheFilePath) < filemtime($this->langFilePath)) {
+            switch ($this->get_file_extension()) {
+                case 'ini':
+                    $config = parse_ini_file($this->langFilePath, true);
+                    break;
+                case 'yml':                   
+                    $config = spyc_load_file($this->langFilePath);
+                    break;
+                case 'json':
+                    $config = json_decode(file_get_contents($this->langFilePath), true);
+                    break;
+                default:
+                    throw new InvalidArgumentException($this->get_file_extension() . " is not a valid extension!");
+            }
+
+            $compiled = "<?php class " . $this->prefix . " {\n";
+            $compiled .= $this->compile($config);
+            $compiled .= 'public static function __callStatic($string, $args) {' . "\n";
+            $compiled .= 'vprintf(constant("self::" . $string), $args);' . "\n";
+            $compiled .= "}\n}";
+
+            if (file_put_contents($this->cacheFilePath, $compiled) === FALSE) {
+                throw new Exception("Could not write cache file to path '" . $this->cacheFilePath . "'. Is it writable?");
+            }
+            chmod($this->cacheFilePath, 0777);
+
+        }
+
+        require_once $this->cacheFilePath;
+    }
+
+    public function isInitialized() {
+        return $this->isInitialized;
+    }
+
+    public function getAppliedLang() {
+        return $this->appliedLang;
+    }
+
+    public function getCachePath() {
+        return $this->cachePath;
+    }
+
+    public function getFallbackLang() {
+        return $this->fallbackLang;
+    }
+
+    public function setFilePath($filePath) {
+        $this->fail_after_init();
+        $this->filePath = $filePath;
+    }
+
+    public function setCachePath($cachePath) {
+        $this->fail_after_init();
+        $this->cachePath = $cachePath;
+    }
+
+    public function setFallbackLang($fallbackLang) {
+        $this->fail_after_init();
+        $this->fallbackLang = $fallbackLang;
+    }
+
+    public function setPrefix($prefix) {
+        $this->fail_after_init();
+        $this->prefix = $prefix;
+    }
+
+    public function setForcedLang($forcedLang) {
+        $this->fail_after_init();
+        $this->forcedLang = $forcedLang;
+    }
+
+    public function setSectionSeperator($sectionSeperator) {
+        $this->fail_after_init();
+        $this->sectionSeperator = $sectionSeperator;
+    }
+
+    /**
+     * getUserLangs()
+     * Returns the user languages
+     * Normally it returns an array like this:
+     * 1. Forced language
+     * 2. Language in $_GET['lang']
+     * 3. Language in $_SESSION['lang']
+     * 4. HTTP_ACCEPT_LANGUAGE
+     * 5. Fallback language
+     * Note: duplicate values are deleted.
+     *
+     * @return array with the user languages sorted by priority.
+     */
+    public function getUserLangs() {
+        $userLangs = array();
+
+        // Highest priority: forced language
+        if ($this->forcedLang != NULL) {
+            $userLangs[] = $this->forcedLang;
+        }
+
+        // 2nd highest priority: GET parameter 'lang'
+        if (isset($_GET['lang']) && is_string($_GET['lang'])) {
+            $userLangs[] = $_GET['lang'];
+        }
+
+        // 3rd highest priority: SESSION parameter 'lang'
+        if (isset($_SESSION['lang']) && is_string($_SESSION['lang'])) {
+            $userLangs[] = $_SESSION['lang'];
+        }
+
+        // 4th highest priority: HTTP_ACCEPT_LANGUAGE
+        if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            foreach (explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']) as $part) {
+                $userLangs[] = strtolower(substr($part, 0, 2));
+            }
+        }
+
+        // Lowest priority: fallback
+        $userLangs[] = $this->fallbackLang;
+
+        // remove duplicate elements
+        $userLangs = array_unique($userLangs);
+
+        foreach ($userLangs as $key => $value) {
+            $userLangs[$key] = preg_replace('/[^a-zA-Z0-9_-]/', '', $value); // only allow a-z, A-Z and 0-9
+        }
+
+        return $userLangs;
+    }
+
+
+    /**
+     * Recursively compile an associative array to PHP code.
+     */
+    protected function compile($config, $prefix = '') {
+        $code = '';
+        foreach ($config as $key => $value) {
+            if (is_array($value)) {
+                $code .= $this->compile($value, $prefix . $key . $this->sectionSeperator);
+            } else {
+                $code .= 'const ' . $prefix . $key . ' = \'' . str_replace('\'', '\\\'', $value) . "';\n";
+            }
+        }
+        return $code;
+    }
+
+    protected function get_file_extension() {
+        return substr(strrchr($this->langFilePath, '.'), 1);
+    }
+
+    protected function fail_after_init() {
+        if ($this->isInitialized()) {
+            throw new BadMethodCallException('This ' . __CLASS__ . ' object is already initalized, so you can not change any settings.');
+        }
+    }
+
+}
